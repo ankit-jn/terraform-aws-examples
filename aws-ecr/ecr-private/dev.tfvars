a0e6d10e264d29877ecd14c0c262d5d34aa4b441
Ankit Jain <ankii.jain@gmail.com>
2022-11-04 12:53:54 +0530

    variable refactoring:

---
+++ aws-ecr/ecr-private/dev.tfvars
@@ -0,0 +1,18 @@
+repositories = [
+    {
+        name = "flight-service"
+        enable_encryption = true
+        enable_image_scan = true
+        create_lifecycle_policy = true
+        lifecycle_policy_file = "flight-service-lifecycle-policy.json"
+        attach_policy = true
+        policy_file = "ecr-policy.json"
+    },
+    {
+        name = "currency-service"
+        enable_encryption = true
+        enable_image_scan = true
+        attach_policy = true
+        policy_file = "ecr-policy.json"
+    }
+]
--- aws-ecr/ecr-private/main.tf
+++ aws-ecr/ecr-private/main.tf
@@ -1,22 +1,7 @@
+variable "repositories" {}
+
 module "ecr" {
     source = "git::https://github.com/arjstack/terraform-aws-ecr.git?ref=v1.0.0"
 
-    repositories = [
-      {
-        name = "flight-service"
-        enable_encryption = true
-        enable_image_scan = true
-        create_lifecycle_policy = true
-        lifecycle_policy_file = "flight-service-lifecycle-policy.json"
-        attach_policy = true
-        policy_file = "ecr-policy.json"
-      },
-      {
-        name = "currency-service"
-        enable_encryption = true
-        enable_image_scan = true
-        attach_policy = true
-        policy_file = "ecr-policy.json"
-      }
-    ]
+    repositories = var.repositories
 }
