67a6317910de349d87f6f364f805cb22b74adc6e
Ankit Jain <ankii.jain@gmail.com>
2022-11-23 11:36:41 +0530

    correct module reference

--- aws-timestream/timestream-complete/main.tf
+++ aws-timestream/timestream-complete/main.tf
@@ -1,6 +1,6 @@
 module "timestream" {
-    # source = "git::https://github.com/arjstack/terraform-aws-timestream?ref=v1.0.0"
-    source = "../../../terraform-aws-timestream"
+    source = "git::https://github.com/arjstack/terraform-aws-timestream?ref=v1.0.0"
+    
     database_name   = var.database_name
     create_kms_key = true
 
