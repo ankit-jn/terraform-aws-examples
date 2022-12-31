e556d46cedfd32cba059591c28b4073d25bdd99b
Ankit Jain <ankii.jain@gmail.com>
2022-12-31 06:23:44 +0530

    source changes

--- aws-secret-manager/secret/main.tf
+++ aws-secret-manager/secret/main.tf
@@ -1,7 +1,6 @@
 module "secret_manager" {
-    # source = "git::https://github.com/arjstack/terraform-aws-secret-manager.git?ref=v1.0.0"
-    source = "../../../terraform-aws-secret-manager"
-
+    source = "git::https://github.com/arjstack/terraform-aws-secret-manager.git?ref=v1.0.0"
+    
     secret_name = var.secret_name
     secret_description = var.secret_description
     secret_values = var.secret_values
